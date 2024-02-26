8e367f8e0537517b6d6846608f37682a27342992
Yury Kareba <wwshjts@gmail.com>
2024-02-26 18:42:41 +0500

    deleted useless code

--- tests/fibHeapUnitTests.cpp
+++ tests/fibHeapUnitTests.cpp
@@ -4,27 +4,8 @@
 
 class FibHeapTest : public testing::Test {
  protected:
-  // You can remove any or all of the following functions if their bodies would
-  // be empty.
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-/*
-  void SetUp() override {
-     // Code here will be called immediately after the constructor (right
-     // before each test).
-  }
-
-  void TearDown() override {
-     // Code here will be called immediately after each test (right
-     // before the destructor).
-  }
-*/
-
     FibHeap h_;
     std::vector<int> v = {1, 2, -42};
-  // Class members declared here can be used by all tests in the test suite
-  // for Foo.
 };
 
 TEST_F(FibHeapTest, InitWithVector) {
